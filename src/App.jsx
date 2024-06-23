5d20d4f1c73e69dd1faec1077702cb5757b5342f
Atakan Temizkan <atikotemizkan@gmail.com>
2024-06-23 19:42:20 +0300

    finished core criteria and ex 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,77 @@
-import "./App.css"
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [otherCount, setOtherCount] = useState(0);
+  const [sign, setSign] = useState("+");
+  const [calculation, setCalculation] = useState(0);
+
+  function addCalcNumber(buttonValue) {
+    setCount(count * 10 + buttonValue);
+  }
+
+  function addOtherCalcNumber(buttonValue) {
+    setOtherCount(otherCount * 10 + buttonValue);
+  }
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{count}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => addCalcNumber(1)}>1</button>
+          <button onClick={() => addCalcNumber(2)}>2</button>
+          <button onClick={() => addCalcNumber(3)}>3</button>
+          <button onClick={() => addCalcNumber(4)}>4</button>
+          <button onClick={() => addCalcNumber(5)}>5</button>
+          <button onClick={() => addCalcNumber(6)}>6</button>
+          <button onClick={() => addCalcNumber(7)}>7</button>
+          <button onClick={() => addCalcNumber(8)}>8</button>
+          <button onClick={() => addCalcNumber(9)}>9</button>
+          <button onClick={() => addCalcNumber(0)}>0</button>
+          <button onClick={() => setCount(count * 0)}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{sign}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          <button onClick={() => setSign("+")}>+</button>
+          <button onClick={() => setSign("-")}>-</button>
+          <button onClick={() => setSign("*")}>*</button>
+          <button onClick={() => setSign("/")}>/</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{otherCount}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => addOtherCalcNumber(1)}>1</button>
+          <button onClick={() => addOtherCalcNumber(2)}>2</button>
+          <button onClick={() => addOtherCalcNumber(3)}>3</button>
+          <button onClick={() => addOtherCalcNumber(4)}>4</button>
+          <button onClick={() => addOtherCalcNumber(5)}>5</button>
+          <button onClick={() => addOtherCalcNumber(6)}>6</button>
+          <button onClick={() => addOtherCalcNumber(7)}>7</button>
+          <button onClick={() => addOtherCalcNumber(8)}>8</button>
+          <button onClick={() => addOtherCalcNumber(9)}>9</button>
+          <button onClick={() => addOtherCalcNumber(0)}>0</button>
+          <button onClick={() => setOtherCount(otherCount * 0)}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{calculation}</p>
         <div>
-          <button>=</button>
+          <button
+            onClick={() => setCalculation(eval(count + sign + otherCount))}
+          >
+            =
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
